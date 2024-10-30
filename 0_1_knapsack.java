de5d3fecf87ff89681bc923fff5bec62e822b414
Sneha Ghone <66209707+sneha1510@users.noreply.github.com>
2024-10-30 14:03:30 +0530

    Update 0_1_knapsack.java

--- 0_1_knapsack.java
+++ 0_1_knapsack.java
@@ -1,44 +1,24 @@
-public class Main
-{
-    // Utility function to return the maximum of two integers
-    static int max(int a, int b) 
-    {
-		return (a > b) ? a : b; // Return the larger of a or b
-	}
-    
-    // Recursive function to solve the 0/1 Knapsack problem
-	static int knapSack(int W, int wt[], int val[], int n) {
-		if (n == 0 || W == 0) {
-			// Base case: If no items are left (n == 0) or the knapsack is full (W == 0), return 0
-			return 0; 
-		}
+class KnapSack {
+    static int knapDP(int W, int wt[], int val[], int n) {
+        int dp[][] = new int[n + 1][W + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0)
+                    dp[i][w] = 0;
+                else if (wt[i - 1] <= w)
+                    dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+                else
+                    dp[i][w] = dp[i - 1][w];
+            }
+        }
+        return dp[n][W];
+    }
 
-		// If the weight of the nth item is greater than the remaining capacity W, exclude this item
-		if (wt[n - 1] > W) {
-			return knapSack(W, wt, val, n - 1);
-		}
-
-		// Otherwise, return the maximum of:
-		// 1. Value obtained by including the nth item (val[n-1] + remaining knapsack capacity)
-		// 2. Value obtained by excluding the nth item
-		else {
-			return max(
-                val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1),  // Include the item
-                knapSack(W, wt, val, n - 1)                             // Exclude the item
-            );
-		}
-	}
-
-    // Main method to test the knapSack function
-	public static void main(String[] args) 
-	{
-		// Example input: values and weights of the items
-		int val[] = new int[] { 60, 100, 120 };  // Values of the items
-		int wt[] = new int[] { 10, 20, 30 };    // Weights of the items
-		int W = 50;                             // Capacity of the knapsack
-		int n = val.length;                     // Number of items
-
-		// Print the result of the knapSack function
-		System.out.println(knapSack(W, wt, val, n));
-	}
+    public static void main(String[] args) {
+        int val[] = {100, 60, 120};
+        int wt[] = {10, 20, 30};
+        int W = 50;
+        int n = val.length;
+        System.out.println("Max profit (DP): " + knapDP(W, wt, val, n));
+    }
 }
